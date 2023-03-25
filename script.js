54d0823f157eac593499263e97e3905f0d925b37
aa <81450053+negi-wasabi@users.noreply.github.com>
2023-03-25 20:32:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-// DOM要素を取得
-const donateInput = document.querySelector('#donate-input');
-const donateButton = document.querySelector('#donate-button');
-const meterFill = document.querySelector('#meter-fill');
-const meterText = document.querySelector('#meter-text');
+const meterFill = document.getElementById("meterFill");
+const progressBar = document.getElementById("progressBar");
+const donateButton = document.getElementById("donateButton");
+const donationInput = document.getElementById("donationInput");
+const nameLabel = document.getElementById("nameLabel");
+const nameInput = document.getElementById("nameInput");
+const logo = document.getElementById("logo");
 
-// 寄付総額の初期値
 let totalDonation = 334;
 
-// メーターとテキストを更新する関数
-function updateMeter() {
-  // メーターを更新
-  meterFill.style.width = `${(totalDonation / 100) % 100}%`;
-  // テキストを更新
-  meterText.innerText = `${totalDonation.toLocaleString()}円`;
+function addDonation() {
+  const donation = parseFloat(donationInput.value);
+  const name = nameInput.value;
+
+  if (donation <= 0) {
+    alert("寄付する金額は1円以上でなければなりません。");
+    return;
+  }
+
+  totalDonation += donation;
+  updateMeter(totalDonation);
+  updateProgressBar(totalDonation);
+  clearInput();
 }
 
-// ボタンがクリックされたら寄付を処理する
-donateButton.addEventListener('click', () => {
-  // 入力値を数値に変換して寄付総額に追加
-  const donationAmount = Number(donateInput.value);
-  totalDonation += donationAmount;
-  // メーターとテキストを更新
-  updateMeter();
-  // 入力欄をリセット
-  donateInput.value = '';
-});
+function clearInput() {
+  donationInput.value = "";
+  nameInput.value = "";
+}
+
+function updateMeter(totalDonation) {
+  meterFill.style.width = `${totalDonation / 100}%`;
+  meterFill.textContent = `¥${totalDonation.toLocaleString()}`;
+}
+
+function updateProgressBar(totalDonation) {
+  const maxDonation = 6482;
+  const percentage = (totalDonation / maxDonation) * 100;
+  progressBar.style.width = `${percentage}%`;
+}
+
+donateButton.addEventListener("click", addDonation);

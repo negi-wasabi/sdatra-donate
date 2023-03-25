9c6970dfe16f30d0ba15427d6cc4b56e9ecaa688
aa <81450053+negi-wasabi@users.noreply.github.com>
2023-03-25 19:14:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
-window.addEventListener("DOMContentLoaded", function() {
-	// 募金総額を表示する要素を取得
-	const totalElement = document.getElementById("total");
-	// メーターを表示する要素を取得
-	const meterElement = document.getElementById("meter");
-	// 寄付フォームを取得
-	const formElement = document.querySelector("form");
-	// 寄付ボタンを取得
-	const donateButton = document.getElementById("donate");
+// DOM要素の取得
+const donateButton = document.querySelector("#donate-btn");
+const amountInput = document.querySelector("#amount-input");
+const totalAmountDisplay = document.querySelector("#total-amount");
+const meterFill = document.querySelector("#meter-fill");
+const nameInput = document.querySelector("#name-input");
 
-	// 現在の募金総額
-	let totalAmount = 334;
+// 募金関連の変数
+let totalAmount = 334;
+let meterMaxValue = 10000;
 
-	// 募金総額を表示
-	totalElement.textContent = totalAmount;
+// 募金金額の最小値と最大値
+const minValue = 0;
+const maxValue = 10000;
 
-	// メーターを更新
-	updateMeter(totalAmount);
+// 募金金額の範囲内に収まるランダムな数値を返す関数
+function getRandomAmount() {
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+}
 
-	// 寄付フォームが送信されたときの処理
-	formElement.addEventListener("submit", function(event) {
-		event.preventDefault();
-		const name = document.getElementById("name").value;
-		const amount = Number(document.getElementById("amount").value);
-		// 寄付金額を加算
-		totalAmount += amount;
-		// 募金総額を更新
-		totalElement.textContent = totalAmount;
-		// メーターを更新
-		updateMeter(totalAmount);
-		// フォームをリセット
-		formElement.reset();
-		alert(name + "さん、" + amount + "円の寄付ありがとうございました！");
-	});
+// 募金ボタンを押したときの処理
+donateButton.addEventListener("click", function () {
+  // 入力された金額を取得
+  let amount = parseInt(amountInput.value);
 
-	// メーターを更新する関数
-	function updateMeter(totalAmount) {
-		// メーターの長さを更新
-		const meterWidth = Math.min(totalAmount
+  // 金額が範囲内に収まるように修正
+  if (isNaN(amount)) {
+    amount = 0;
+  }
+  if (amount > maxValue) {
+    amount = maxValue;
+  }
+  if (amount < minValue) {
+    amount = 0;
+  }
+
+  // 募金金額を更新
+  totalAmount += amount + getRandomAmount();
+
+  // メーターを更新
+  const percent = (totalAmount / meterMaxValue) * 100;
+  meterFill.style.width = `${percent}%`;
+
+  // 募金総額を表示
+  totalAmountDisplay.textContent = `${totalAmount}円`;
+
+  // 名前が入力されている場合はメッセージを表示
+  if (nameInput.value !== "") {
+    alert(`${nameInput.value}さん、ありがとうございました！`);
+  }
+
+  // 入力フォームをリセット
+  amountInput.value = "";
+  nameInput.value = "";
+});
